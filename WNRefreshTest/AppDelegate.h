8e17b9b8176bb3f0f2053432ab8ba3f9982fe646
wangning <316835453@qq.com>
2016-08-19 13:39:31 +0800

    Initial Commit

---
+++ WNRefreshTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WNRefreshTest
+//
+//  Created by 汪宁 on 16/8/19.
+//  Copyright © 2016年 ZHENAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WNRefreshTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WNRefreshTest
+//
+//  Created by 汪宁 on 16/8/19.
+//  Copyright © 2016年 ZHENAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
